9d83abd4aed2b916aaf18e799abdf6609c376db7
Vojtěch Štverák <stverak.vojtech@ssakhk.cz>
2026-05-11 14:26:22 +0200

    -/

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://tickets-app-frontend-master-2.vercel.app/";
-
+  const baseUrl = "https://tickets-app-frontend-master-2.vercel.app";
   return [
     {
       url: `${baseUrl}`,
